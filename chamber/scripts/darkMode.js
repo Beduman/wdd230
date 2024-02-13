d90066ccd48f3d28348941a63076ef8571e73f06
Beduman <behunz77@gmail.com>
2024-02-13 15:42:16 -0700

    better dark mode

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -3,11 +3,11 @@ const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("Dark Mode")) {
-		main.style.background = "#000";
+		main.style.background = "#000807";
 		main.style.color = "#fff";
 		modeButton.textContent = "Light Mode";
 	} else {
-		main.style.background = "#eee";
+		main.style.background = "#fef9ef";
 		main.style.color = "#000";
 		modeButton.textContent = "Dark Mode";
 	}
