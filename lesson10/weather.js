7e71f12cce8ae8de567c9924dd654acd5bd6a376
Beduman <behunz77@gmail.com>
2024-03-13 20:33:41 -0700

    api test

---
+++ lesson10/weather.js
@@ -0,0 +1,33 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=df85fd3eae7d9b8211a7c6efca0497ad';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].______;
+    weatherIcon.setAttribute('src', iconURL);
+    weatherIcon.setAttribute('src', iconURL);
+    captionDesc.textContent = `${desc}`;
+    currentTemp.textContent = "" + Math.round(data.main.temp);
+  }
