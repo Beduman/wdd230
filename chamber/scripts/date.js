cf5e92922cfe8e35fbc078b7a6a86a5691849df8
Beduman <behunz77@gmail.com>
2024-05-18 21:01:10 -0700

    times visited

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -5,28 +5,25 @@ const theDateToday = new Date();
 
 function getLastModified() {
     const lastModified = new Date(document.lastModified).toGMTString();
-    document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
+    document.getElementById("lastModified").innerHTML = lastModified;
+}
+getLastModified();
+
+
+const visitsDisplay = document.querySelector("#lastVisited");
+
+// 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
 }
 
-getLastModified()
+// 4️⃣ increment the number of visits by one.
+numVisits++;
 
-// initialize display elements
-const todayElement = document.querySelector("#today");
-const christmasElement = document.querySelector("#christmas");
-const christmasDateElement = document.querySelector("#christmasDate");
-const daysElement = document.querySelector("#daysleft");
-
-// processing
-const today = Date.now();
-const christmasDate = new Date(Date.UTC(theDateToday.getFullYear(), 11, 25));
-// check if is the waing days of December, if so ... change to next year.
-if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
-	christmasDate.setFullYear(christmasDate.getFullYear() + 1);
-}
-// find difference between epoch times in ms and convert to days
-let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
-
-todayElement.textContent = today;
-christmasElement.textContent = christmasDate.getTime();
-christmasDateElement.textContent = christmasDate;
-daysElement.textContent = `${daysleft.toFixed(0)} days`;
+// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
