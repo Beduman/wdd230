db758486f537e93a87f2b54677a022103ed1ca6d
Beduman <behunz77@gmail.com>
2024-06-29 17:44:42 -0600

    .

--- chamber/scripts/retrieve.js
+++ chamber/scripts/retrieve.js
@@ -52,15 +52,11 @@ getData()
         });
     }
 
-
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector("article");
 
-
-
 gridbutton.addEventListener("click", () => {
-
 	display.classList.add("grid");
 	display.classList.remove("list");
 });
